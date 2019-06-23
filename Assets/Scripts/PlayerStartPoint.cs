78d7de103ac7d64d1012aafb037b5de64fcfdbae
michaeljfalkenstein@gmail.com <michaeljfalkenstein@gmail.com>
2019-06-23 10:06:13 -0400

    Fixed movement and level transitions
    Collisions now work properly on all levels.
    
    Also added in level transition, fixed duplicate player bug, added in
    pre-defined points for the player to start in when entering a new level.

---
+++ Assets/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartPoint : MonoBehaviour {
+
+    private PlayerController player;
+    private CameraController camera;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = FindObjectOfType<PlayerController>();
+        camera = FindObjectOfType<CameraController>();
+
+        player.transform.position = transform.position;
+        camera.transform.position = new Vector3(transform.position.x, 
+                                                transform.position.y, 
+                                                camera.transform.position.z);
+    }
+}
