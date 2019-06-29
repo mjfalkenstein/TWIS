89c8e2a395c97498b52d548e13fd437568f51fc9
michaeljfalkenstein@gmail.com <michaeljfalkenstein@gmail.com>
2019-06-29 18:04:26 -0400

    Setup for multiple characters

---
+++ Assets/Scripts/InstanceScripts/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour { 
+
+    public int maxHealth = 10;
+    public int currentHealth;
+    public Sprite northSprite;
+    public Sprite eastSprite;
+    public Sprite southSprite;
+    public Sprite westSprite;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
